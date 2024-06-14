a241bdc56d4ec1f04023d74a1169b1be07496c6c
Mohammed Malleck <mohammedmalleck@gmail.com>
2024-06-14 22:00:29 +0400

    Feat(Center  the player):  Create a function to center the player in the container
    — use only the  width of the player and the container for this calculation , since  in the layout players hame same height as width and same for the containers(jail,boxes)

--- script/game.js
+++ script/game.js
@@ -36,8 +36,8 @@ function handlePlayersDefaultPosition(){
     const left = coordinatesObject[`left${playerNum}`];
     const top = coordinatesObject[`top${playerNum}`];
 
-    playerEl.style.left = `${(left + ((playerContainerWidth/2) - (playerWidth/2))).toFixed(3)}px`
-    playerEl.style.top = `${(top + ((playerContainerWidth/2) - (playerWidth/2))).toFixed(3)}px`;
+    playerEl.style.left = `${calculateCenter(left,playerContainerWidth,playerWidth)}px`
+    playerEl.style.top = `${calculateCenter(top,playerContainerWidth,playerWidth)}px`;
   });
 };
 
@@ -46,21 +46,22 @@ function setBoxesCoordinates(){
   const boxesObj = {coordinatesName : 'boxes'};
   const homeBoxesObj = {coordinatesName : 'homeBoxes'};
 
-  const {width : playerWidth , height : playerHeight}= document.querySelector('.player').getBoundingClientRect(); 
+  const  playerWidth = document.querySelector('.player').clientWidth; 
 
   document.querySelectorAll('.box').forEach( box =>{
-    const {top ,left , width : boxWidth , height : boxHeight} = box.getBoundingClientRect();
+    const {top ,left , width : boxWidth} = box.getBoundingClientRect();
     const {homeBox,boxNum} = box.dataset;
     //if the box is a home box add the 
     //coordinates of the box in the home box object
     //and vice versa
     if(homeBox){
-      homeBoxesObj[`boxTop${boxNum}`] = `${(top + ((boxHeight / 2) - (playerHeight / 2))).toFixed(3)}px`;
-      homeBoxesObj[`boxLeft${boxNum}`] = `${(left + ((boxWidth / 2) - (playerWidth / 2))).toFixed(3)}px`;
+      homeBoxesObj[`boxTop${boxNum}`] = `${calculateCenter(top,boxWidth,playerWidth)}px`;
+      homeBoxesObj[`boxLeft${boxNum}`] = `${calculateCenter(left,boxWidth,playerWidth)}px`;
       homeBoxesObj[`boxEl${boxNum}`] = box;
+      homeBoxesObj[`boxHomeName${boxNum}`]  = box.classList[1];
     }else{
-      boxesObj[`boxTop${boxNum}`] = `${(top + ((boxHeight / 2) - (playerHeight / 2))).toFixed(3)}px`;
-      boxesObj[`boxLeft${boxNum}`] = `${(left + ((boxWidth / 2) - (playerWidth / 2))).toFixed(3)}px`;
+      boxesObj[`boxTop${boxNum}`] = `${calculateCenter(top,boxWidth,playerWidth)}px`;
+      boxesObj[`boxLeft${boxNum}`] = `${calculateCenter(left,boxWidth,playerWidth)}px`;
       boxesObj[`boxEl${boxNum}`] = box;
     }   
   });
@@ -122,12 +123,11 @@ function movePlayerToBoard(playerEl){
   const playerElTop = Number(top.replace('px',''));
   const playerElLeft = Number(left.replace('px',''));
   const playerElWidth = playerEl.clientWidth;
-  const playerElHeight = playerEl.clientHeight;
   //get the respective player in the header and its rect values
   //get the values to center the player with the respective player in the header
-  const {top : scoreSvgTop,left : scoreSvgLeft,width : scoreSvgWidth,height : scoreSvgHeight} =  document.querySelector(`li.${home} > svg`).getBoundingClientRect();
-  const scoreSvgCenterTop = (scoreSvgTop + ((scoreSvgHeight/2) - (playerElHeight/2))).toFixed(3);
-  const scoreSvgCenterLeft = (scoreSvgLeft + ((scoreSvgWidth/2) - (playerElWidth/2))).toFixed(3);
+  const {top : scoreSvgTop,left : scoreSvgLeft,width : scoreSvgWidth} =  document.querySelector(`li.${home} > svg`).getBoundingClientRect();
+  const scoreSvgCenterTop = calculateCenter(scoreSvgTop,scoreSvgWidth,playerElWidth);
+  const scoreSvgCenterLeft = calculateCenter(scoreSvgLeft,scoreSvgWidth,playerElWidth);
   //set the css variables on the player responsible for the animation
   playerEl.style.setProperty("--current-top", (playerElTop - 50).toFixed(3) + "px");
   playerEl.style.setProperty("--current-left", (playerElLeft - 50).toFixed(3) + "px");
@@ -136,10 +136,12 @@ function movePlayerToBoard(playerEl){
   //add the class to enable animation
   playerEl.classList.add('done');
 }
+function calculateCenter(offsetValue,containerWidth,playerWidth){
+  return (offsetValue + (containerWidth/2) - (playerWidth/2)).toFixed(3);
+}
 
 handlePlayersDefaultPosition();
 setBoxesCoordinates();
-console.log(coordinatesArray)
 document.querySelector('[data-roll-turn="1"]').classList.add('point');
 
 window.addEventListener('resize',()=>{
